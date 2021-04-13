7bacc49e0c6d4bc40a8ba9a6bc4d3d9d6737414e
DESKTOP-ES90GA7\ATTOUMANI <hakimeattoumani.simplon@gmail.com>
2021-04-13 16:38:31 +0200

    exeperience js

--- js/app.js
+++ js/app.js
@@ -1,35 +1,44 @@
 document.getElementById("inscription").addEventListener("submit",function(e) {
-    e.preventDefault();
+    
 
     var erreur;
-    var pseudo = document.getElementById("pseudo");
-    var email = document.getElementById("email");
-    var email2 = document.getElementById("email2");
-    var mdp = document.getElementById("mdp");
+    var inputs = this.getElementsByTagName("input");
 
-    if (!mdp.value){
-        erreur = "veuillez renseigner un mot de passe";
+    for (var i = 0; i<inputs.length;i++){
+        if(!inputs[i].value){
+
+            erreur ="Veuillez renseigner tous les champs"
+        }
     }
-    if (!email.value){
-        erreur = "veuillez renseigner un email";
-    }
-    if (!email2.value){
-        erreur = "veuillez renseigner a nouveau l'email";
-    }
-    if (!pseudo.value){
-        erreur = "veuillez renseigner un pseudo";
-    }
-
-
-
-
+    
     if (erreur) {
         document.getElementById("erreur").innerHTML = erreur;
         return false;
     }else {
-        alert
+        alert("formulaire envoyé !")
     }
 
-     alert("formulaire envoyé !")
 });
 
+    // var pseudo = document.getElementById("pseudo");
+    // var email = document.getElementById("email");
+    // var email2 = document.getElementById("email2");
+    // var mdp = document.getElementById("mdp");
+
+    // if (!mdp.value){
+    //     erreur = "veuillez renseigner un mot de passe";
+    // }
+    // if (!email.value){
+    //     erreur = "veuillez renseigner un email";
+    // }
+    // if (!email2.value){
+    //     erreur = "veuillez renseigner a nouveau l'email";
+    // }
+    // if (!pseudo.value){
+    //     erreur = "veuillez renseigner un pseudo";
+    // }
+
+
+
+
+    
